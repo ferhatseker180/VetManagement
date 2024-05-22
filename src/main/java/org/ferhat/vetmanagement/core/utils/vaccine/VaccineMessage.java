c64330ac817a4d84e53c6ae4fea9bb1e240815df
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-05-22 23:57:40 +0300

    Her sınıf için ayrı message yapısı ve result helper oluşturuldu

---
+++ src/main/java/org/ferhat/vetmanagement/core/utils/vaccine/VaccineMessage.java
@@ -0,0 +1,11 @@
+package org.ferhat.vetmanagement.core.utils.vaccine;
+
+public class VaccineMessage {
+
+    public static final String CREATED = "Vaccine Created";
+    public static final String DELETED = "Vaccine Deleted";
+    public static final String VALIDATE_ERROR = "Vaccine Validation Error";
+    public static final String OK = "Vaccine Processing is successful";
+    public static final String NOT_FOUND = "Vaccine not found";
+    public static final String EXISTING_VACCINE = "Vaccine already exists";
+}
